c03a1c265f54c8bcdee37d5cd52d3bceed69dbb2
MoD3223 <patryk4610@gmail.com>
2019-02-25 23:51:15 +0100

    Add project files.

---
+++ Variables.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCalculator
+{
+    class Variables
+    {
+        //Longs
+        public static long AddL(long k, long j)
+        {
+            return k + j;
+        }
+        public static long SubL(long k, long j)
+        {
+            return k - j;
+        }
+        public static long MultL(long k, long j)
+        {
+            return k * j;
+        }
+        public static long DivL(long k, long j)
+        {
+            return k / j;
+        }
+
+        //Doubles
+        public static double AddD(double k, double j)
+        {
+            return k + j;
+        }
+        public static double SubD(double k, double j)
+        {
+            return k - j;
+        }
+        public static double MultD(double k, double j)
+        {
+            return k * j;
+        }
+        public static double DivD(double k, double j)
+        {
+            return k / j;
+        }
+
+
+    }
+}
